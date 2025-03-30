e4abf5b87dcaa930a309ff07a651b09b0a8edde8
els <els@no.spam>
2025-03-30 15:22:38 +0200

    Fixed typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ( {wordsLength} ) => {
                     <a href="#">
                         <h1 className="text-4xl font-bold text-center">PERFECTUM</h1>
                     </a>
-                    <h2 className="text-lg text-center">Er staan {wordsLength} speciale woorden in dit test.</h2>
+                    <h2 className="text-lg text-center">Er staan {wordsLength} speciale woorden in deze test.</h2>
                     <h2 className="text-lg text-center">Vul de juiste vorm van <b>het perfectum</b> in.</h2>
                     <h2 className="text-center">(De woordjes staan in het leerboek <i>Zo gezegd 1.2 mondeling</i> op pagina 165 en 166)</h2>
             </div>
