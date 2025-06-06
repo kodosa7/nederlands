65d63782a4dc7478aedc418d1a0f1464b1fe6831
els <els@no.spam>
2025-06-06 09:36:07 +0200

    New text addition in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = ( {wordsLength} ) => {
         <header className="container mb-4 position-relative">
             <div className="row pt-4">
                     <a href="#">
-                        <h1 className="text-4xl font-bold text-center">PERFECTUM</h1>
+                        <h1 className="text-4xl font-bold text-center">IRREGULIERE VERBA: PERFECTUM</h1>
                     </a>
                     <h2 className="text-lg text-center">Er staan {wordsLength} speciale woorden in deze test.</h2>
                     <h2 className="text-lg text-center">Vul de juiste vorm van <b>het perfectum</b> in.</h2>
