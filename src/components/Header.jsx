9bf66a2c8d8b5e42e2f1d5128bb47e6d4a396384
els <els@no.spam>
2025-06-06 09:39:06 +0200

    New text addition in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ( {wordsLength} ) => {
                         <h1 className="text-4xl font-bold text-center">IRREGULIERE VERBA: PERFECTUM</h1>
                     </a>
                     <h2 className="text-lg text-center">Er staan {wordsLength} speciale woorden in deze test.</h2>
-                    <h2 className="text-lg text-center">Vul de juiste vorm van <b>het perfectum</b> in.</h2>
+                    <h2 className="text-lg text-center">Vul de juiste vorm van <b>het perfectum</b> in. Klik vervolgens op OK of druk op Enter.</h2>
                     <h2 className="text-center">(De woordjes staan in de leerboeken <i>Zo gezegd 2.1 schriftelijk persoonlijk</i> op pagina 147 en <i>publiek</i> op pagina 170)</h2>
             </div>
         </header>
