32b8ce4173292e0a77e5a3f880123069e6482f29
els <els@no.spam>
2026-01-29 15:40:41 +0100

    Added new words for the impoerfectum section; added a PDF with the word list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import woordenLijstFile from "onregelmatige_verba.pdf"
+
 const Header = ( {wordsLength} ) => {
     return (
         <header className="container mb-4 position-relative">
@@ -5,7 +7,13 @@ const Header = ( {wordsLength} ) => {
                     <a href="#">
                         <h1 className="text-4xl font-bold text-center">IRREGULIERE VERBA: PERFECTUM & IMPERFECTUM</h1>
                     </a>
-                    <h2 className="text-center">(De woordjes staan in de leerboeken <i>Zo gezegd 2.1 schriftelijk persoonlijk</i> op pagina 147 en <i>publiek</i> op pagina 170)</h2>
+                    <h2 className="text-center">
+                        (De woordjes staan 
+                            <a href={ woordenLijstFile }>
+                                hier
+                            </a>
+                            (PDF) en in de leerboeken <i>Zo gezegd 2.1 schriftelijk persoonlijk</i> op pagina 147 en <i>publiek</i> op pagina 170)
+                    </h2>
             </div>
         </header>
     )
