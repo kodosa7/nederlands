f11dc1402ba183f3ffe1803505f54421a30cdb9e
els <els@no.spam>
2026-01-29 16:47:28 +0100

    Fixed text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = ( {wordsLength} ) => {
                         <h1 className="text-4xl font-bold text-center">IRREGULIERE VERBA: PERFECTUM & IMPERFECTUM</h1>
                     </a>
                     <h2 className="text-center">
-                        (De woordjes staan b.v. <a href={ woordenLijstFile }>hier</a> (PDF) en in de leerboeken <i>Zo gezegd 2.3 mondeling persoonlijk</i> op pagina's 234–236 en <i>publiek</i> op pagina's 266–268)
+                        (De woordjes staan b.v. <a href={ woordenLijstFile }>hier</a> (PDF) en in de leerboeken <i>Zo gezegd 2.3–2.4 mondeling persoonlijk</i> op pagina's 234–236 en <i>publiek</i> op pagina's 266–268)
                     </h2>
             </div>
         </header>
